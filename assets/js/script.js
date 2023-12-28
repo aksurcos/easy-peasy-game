40fa4bf8d3d9cce84f88b4f86cf74650794ef77f
aksurcos <>
2023-12-28 20:30:39 +0000

    correcting

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@
         {
             let button = document.createElement('button');
             button.appendChild(document.createTextNode(buttons[i].icon));
-            document.getElementById('game').appendChild(button);
+            document.getElementById('game-area').appendChild(button);
             button.addEventListener('click', function() { choice(i); });
         }
         
@@ -20,25 +20,25 @@
         {
             let you = buttons[index];
             let computer = buttons[Math.floor(Math.random() * buttons.length)];
-            let result = document.getElementById('result');
+            let outcome = document.getElementById('outcome');
             
             if(you.beats.hasOwnProperty(computer.name))
             {
-                increase('won');
-                result.innerHTML = 'You beat the computer! <i class="fa-solid fa-heart"></i><br>';
-                result.innerHTML += [you.icon, you.name, you.beats[computer.name], computer.name, computer.icon].join(' ');
+                increase('win');
+                outcome.innerHTML = 'You beat the computer! <i class="fa-solid fa-heart"></i><br>';
+                outcome.innerHTML += [you.icon, you.name, you.beats[computer.name], computer.name, computer.icon].join(' ');
             }
             else if(computer.beats.hasOwnProperty(you.name))
             {
-                increase('lost');
-                result.innerHTML = 'The computer beat you! <i class="fa-solid fa-heart-crack"></i>  <br>';
-                result.innerHTML += [computer.icon, computer.name, computer.beats[you.name], you.name, you.icon].join(' ');
+                increase('lose');
+                outcome.innerHTML = 'The computer beat you! <i class="fa-solid fa-heart-crack"></i>  <br>';
+                outcome.innerHTML += [computer.icon, computer.name, computer.beats[you.name], you.name, you.icon].join(' ');
             }
             else
             {
                 increase('draw');
-                result.innerHTML = 'Draw! <i class="fa-solid fa-handshake"></i><br>';
-                result.innerHTML += [computer.icon, 'Nobody Wins.', you.icon].join(' ');
+                outcome.innerHTML = 'Draw! <i class="fa-solid fa-handshake"></i><br>';
+                outcome.innerHTML += [computer.icon, 'You both chose the same!', you.icon].join(' ');
             }
         }
         
@@ -47,5 +47,7 @@
             let element = document.getElementById(id);
             element.innerHTML = Number(element.innerHTML) + 1;
         }
+
+       
     
    
